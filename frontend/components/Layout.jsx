80fbd3d73eeb4c0536e1972e137687b14fa84cbb
Guille Cánovas <755848@unizar.es>
2022-09-22 11:37:19 +0200

    Layout.jsx

--- frontend/components/Layout.jsx
+++ frontend/components/Layout.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React from 'react';
+import Head from 'next/head';
 
-const Layout = () => {
-    return (
-        <div>
-            
-        </div>
-    )
+import Navbar from './Navbar';
+import Footer from './Footer';
+
+// children no es algo que le pasamos
+// children es algo que permite react para identificar a lo que esta contenido dentro de otro
+// de esta manera accedemos a los componentes 
+
+const Layout = ({ children }) => {
+  return (
+    <div className="layout">
+      <Head>
+        <title>Guillermo Cánovas</title> {/* titulo de la pagina */}
+      </Head>
+      <header>
+        <Navbar />
+      </header>
+      <main className="main-container">
+        {children}
+      </main>
+      <footer>
+        <Footer />
+      </footer>
+    </div>
+  )
 }
 
 export default Layout
--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,8 +1,14 @@
 import React from 'react'
+
+import { Layout } from '../components';
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Component {...pageProps} /> {/* el componente en el que estamos */}
+    </Layout>
+  )
 }
 
 export default MyApp
